feb715137f988f61cad2c872865930aad6f74c15
Giuliotheo <g.theodoli99@gmail.com>
2025-04-07 12:28:05 +0200

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,11 @@
+print("hello")
+nome = "Giulio"
+eta = 25
+print("il mio nome è", nome, "la mia eta è eta", "anni.")
+
+nome = input("inserisci il tuo nome")
+eta = int( input("inserisci la tua eta") )
+
+print("il mio nome è " + nome + " la mia eta è " , eta)
+
+print(1 + 5)
