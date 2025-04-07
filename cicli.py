703c92eebdcaa4a0571761fc84c82206a24769f8
Giuliotheo <g.theodoli99@gmail.com>
2025-04-07 17:42:55 +0200

    Create cicli.py

---
+++ cicli.py
@@ -0,0 +1,22 @@
+
+
+controllore = True
+
+while controllore:
+    print(1)
+    
+    controllore = False
+    
+    conteggio = 0
+    
+    while conteggio < 5:
+        print(conteggio)
+        conteggio += 1
+        
+    
+        # blocco di codice
+        
+        numeri = [1, 2, 3, 4]
+        
+        for numero in numeri:
+            print(numero)
